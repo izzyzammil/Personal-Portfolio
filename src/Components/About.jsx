4b9b9fd50a6ca85f59ab4dddac8575cf916531d4
unknown <izzy.zammil@gmail.com>
2022-06-07 23:21:19 +0700

    Add About Section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
-import ImageAbout from "../assets/image/Zammil-about2.jpg";
+import ImageAbout from "../assets/image/Zammil-about3.png";
 
 const About = () => {
   return (
     <Container className="py-5">
-      <Row>
-        <Col lg={5} className="d-flex justify-content-center">
-          <Image src={ImageAbout} width={320} roundedCircle />
+      <Row className="d-flex align-items-center">
+        <Col
+          lg={5}
+          className="d-flex justify-content-center"
+          style={{ position: "relative", top: -12 }}
+        >
+          <Image src={ImageAbout} width={340} />
         </Col>
         <Col lg={7}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          consectetur dolore dolores? Dolores impedit, quaerat nesciunt modi nam
-          fugit saepe fugiat assumenda minima mollitia nobis consequatur
-          perspiciatis voluptate non veritatis quas nostrum error illo labore
-          temporibus harum eos. Quisquam, obcaecati voluptate! Delectus quaerat
-          doloremque dolorum nobis aspernatur tempore exercitationem quis.
+          <div className="about-name">
+            <span>WHO I AM</span>
+          </div>
+          <div className="mt-2">
+            <span className="fs-2 fw-bold">About Me</span>
+          </div>
+          <div className="mt-3">
+            <p className="about-description">
+              Hi! I’m Muzammil, and I’m a designer & developer who has passion
+              for building clean web applications with intuitive functionality.
+              I enjoy the process of turning ideas into reality using creative
+              solutions. I’m always curious about learning new skills, tools,
+              and concepts. In addition to working on various solo full stack
+              projects, I have worked with creative teams, which involves daily
+              stand-ups and communications, source control, and project
+              management.
+            </p>
+          </div>
+          <div className="button about-button mt-4">Hire Me</div>
         </Col>
       </Row>
     </Container>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Image } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import ImageHero from "../assets/image/Muzammil.png";
+import Abstract from "../assets/image/abstract.png";
 import Github from "@iconscout/react-unicons/icons/uil-github";
 import LinkedIn from "@iconscout/react-unicons/icons/uil-linkedin";
 import Instagram from "@iconscout/react-unicons/icons/uil-instagram";
@@ -9,6 +10,26 @@ import Whatsapp from "@iconscout/react-unicons/icons/uil-whatsapp";
 const Hero = () => {
   return (
     <Container fluid className="hero-bg">
+      <div>
+        <Image
+          src={Abstract}
+          width={360}
+          style={{
+            position: "absolute",
+            left: 50,
+            top: 320,
+            opacity: 0.5,
+            zIndex: 1,
+          }}
+        />
+
+        <Image
+          src={Abstract}
+          width={360}
+          style={{ position: "absolute", right: 30, opacity: 0.5, zIndex: 1 }}
+        />
+      </div>
+
       <div className="py-5 d-flex flex-column align-items-center">
         <Image width={210} src={ImageHero} className="h-image" />
         <div className="mt-4">
